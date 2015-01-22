14d5811eb8f064a1fc77daf8a3c4447b810a02e4
damianedwards <damian@damianedwards.com>
2015-01-21 17:47:38 -0800

    Initial cut of LinkTagHelper: - Core functionality works, need to add property value checks, tests, etc. - TagHelperSample.Web updated to use layout pages & link tag helper to demo loading a CSS file - TagHelperSample.Web updated to use error page & static files so "k web" works properly - #1580

--- samples/TagHelperSample.Web/Startup.cs
+++ samples/TagHelperSample.Web/Startup.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc;
 using Microsoft.Framework.DependencyInjection;
 
@@ -9,21 +10,26 @@ namespace TagHelperSample.Web
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+                
+            // Setup services with a test AssemblyProvider so that only the sample's assemblies are loaded. This
+            // prevents loading controllers from other assemblies when the sample is used in functional tests.
+            services.AddTransient<IAssemblyProvider, TestAssemblyProvider<Startup>>();
+
+            services.Configure<MvcOptions>(options =>
+            {
+                options.AddXmlDataContractSerializerFormatter();
+            });
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            app.UseServices(services =>
-            {
-                services.AddMvc();
-                
-                // Setup services with a test AssemblyProvider so that only the sample's assemblies are loaded. This
-                // prevents loading controllers from other assemblies when the sample is used in functional tests.
-                services.AddTransient<IAssemblyProvider, TestAssemblyProvider<Startup>>();
+            app.UseErrorPage();
 
-                services.Configure<MvcOptions>(options =>
-                {
-                    options.AddXmlDataContractSerializerFormatter();
-                });
-            });
+            app.UseStaticFiles();
+
             app.UseMvc();
         }
     }
